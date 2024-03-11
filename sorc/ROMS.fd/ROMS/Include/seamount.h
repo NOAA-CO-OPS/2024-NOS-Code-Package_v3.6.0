eb062b3c15eaf9a7606867eb7171dd5df2cdf944
CristinaUrizar <Cristina.Urizar@noaa.gov>
2024-03-11 16:59:03 +0000

    adding sorc ROMS

---
+++ sorc/ROMS.fd/ROMS/Include/seamount.h
@@ -0,0 +1,32 @@
+/*
+** git $Id$
+** svn $Id: seamount.h 1151 2023-02-09 03:08:53Z arango $
+*******************************************************************************
+** Copyright (c) 2002-2023 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.md                                                     **
+*******************************************************************************
+**
+** Options for Seamount Test.
+**
+** Application flag:   SEAMOUNT
+** Input script:       roms_seamount.in
+*/
+
+#define UV_ADV
+#define UV_COR
+#define UV_QDRAG
+#define UV_VIS2
+#define MIX_S_UV
+#define DJ_GRADPS
+#define SPLINES_VDIFF
+#define SPLINES_VVISC
+#define TS_DIF2
+#define MIX_GEO_TS
+#define SOLVE3D
+#define ANA_DIAG
+#define ANA_GRID
+#define ANA_INITIAL
+#define ANA_SMFLUX
+#define ANA_STFLUX
+#define ANA_BTFLUX
---
+++ sorc/ROMS.fd/ROMS/Nonlinear/Biology/hypoxia_srm_wrt.h
@@ -0,0 +1,27 @@
+/*
+** git $Id$
+** svn $Id: hypoxia_srm_wrt.h 1151 2023-02-09 03:08:53Z arango $
+*************************************************** Hernan G. Arango ***
+** Copyright (c) 2002-2023 The ROMS/TOMS Group                        **
+**   Licensed under a MIT/X style license                             **
+**   See License_ROMS.md                                              **
+************************************************************************
+**                                                                    **
+**  Writes Hypoxia Simple Respiration Model input parameters into     **
+**  output NetCDF files. It is included in routine "wrt_info.F".      **
+**                                                                    **
+************************************************************************
+*/
+
+!
+!  Write out Hypoxia Simple Respiration Model parameters.
+!
+      CALL netcdf_put_ivar (ng, model, ncname, 'BioIter',               &
+     &                      BioIter(ng), (/0/), (/0/),                  &
+     &                      ncid = ncid)
+      IF (FoundError(exit_flag, NoError, __LINE__, MyFile)) RETURN
+
+      CALL netcdf_put_fvar (ng, model, ncname, 'ResRate',               &
+     &                      ResRate(ng), (/0/), (/0/),                  &
+     &                      ncid = ncid)
+      IF (FoundError(exit_flag, NoError, __LINE__, MyFile)) RETURN
