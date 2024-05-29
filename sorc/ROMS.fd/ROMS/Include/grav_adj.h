e2ad28b445160bd84541b4c6e026c2bd8a34a5ab
CristinaUrizar <Cristina.Urizar@noaa.gov>
2024-05-29 19:40:20 +0000

    Adding ROMS.fd to sorc

---
+++ sorc/ROMS.fd/ROMS/Include/grav_adj.h
@@ -0,0 +1,34 @@
+/*
+** git $Id$
+** svn $Id: grav_adj.h 1151 2023-02-09 03:08:53Z arango $
+*******************************************************************************
+** Copyright (c) 2002-2023 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.md                                                     **
+*******************************************************************************
+**
+** Options for Gravitational Adjustment Test.
+**
+** Application flag:   GRAV_ADJ
+** Input script:       roms_grav_adj.in
+*/
+
+#define UV_ADV
+#define UV_VIS2
+#define UV_LDRAG
+#define MIX_S_UV
+#define DJ_GRADPS
+#define SPLINES_VDIFF
+#define SPLINES_VVISC
+#define TS_DIF2
+#define MIX_S_TS
+#define SOLVE3D
+#define AVERAGES
+#define ANA_GRID
+#define ANA_INITIAL
+#define ANA_SMFLUX
+#define ANA_STFLUX
+#define ANA_BTFLUX
+#define OUT_DOUBLE
+#define DIAGNOSTICS_TS
+#define DIAGNOSTICS_UV
---
+++ sorc/ROMS.fd/ROMS/Include/soliton.h
@@ -0,0 +1,24 @@
+/*
+** git $Id$
+** svn $Id: soliton.h 1151 2023-02-09 03:08:53Z arango $
+*******************************************************************************
+** Copyright (c) 2002-2023 The ROMS/TOMS Group                               **
+**   Licensed under a MIT/X style license                                    **
+**   See License_ROMS.md                                                     **
+*******************************************************************************
+**
+** Options for Equatorial Soliton Test.
+**
+** Application flag:   SOLITON
+** Input script:       roms_soliton.in
+*/
+
+#define UV_ADV
+#define UV_C4ADVECTION
+#define UV_VIS2
+#define UV_COR
+#define UV_QDRAG
+#define AVERAGES
+#define ANA_GRID
+#define ANA_INITIAL
+#define ANA_SMFLUX
